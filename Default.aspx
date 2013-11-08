1b3ad96d6148aa63f29c7220a4680c01ec6a2a91
Jonathan M. Tran <jmt@jonathanmtran.com>
2013-11-07 23:57:05 -0800

    Project 1

---
+++ Default.aspx
@@ -0,0 +1,139 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/rwm_ecommerce.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server"></asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="default" Runat="Server">
+	<div class="container">
+		<div class="row">
+			<div id="SubCategories" class="span3" runat="server">
+				<div class="well">
+					<ul class="nav nav-list">
+						<li class="nav-header">Sidebar</li>
+						<% Create_Sidebar(); %>
+					</ul>
+				</div>
+			</div>
+
+			<div class="span<% Response.Write(SubCategories.Visible == false ? 12 : 9); %>">
+
+			<asp:Panel ID="index" Visible="false" runat="server">
+				<div class="hero-unit">
+					<h1 class="">Special Offer</h1>
+					<p class="">here is the best offer of the month! Do not loose it!</p>
+					<p><a href="#" class="btn btn-primary btn-large">Learn more »</a></p>
+				</div>
+
+				<ul class="thumbnails">
+					<li class="span3">
+						<div class="thumbnail">
+							<img src="holder.js/300x200" alt="">
+							<div class="caption">
+								<h4>Thumbnail label</h4>
+								<p><strike>Euro 150,00</strike>&nbsp;Euro 100,00</p>
+								<a class="btn btn-primary" href="#">View</a>
+								<a class="btn btn-success" href="#">Add to Cart</a>
+							</div>
+						</div>
+					</li>
+				</ul>
+
+				<!--<div class="pagination">
+					<ul>
+						<li class"disabled"><span>Prev</span></li>
+						<li class"disabled"><span>1</span></li>
+						<li><a href="#">2</a></li>
+						<li><a href="#">3</a></li>
+						<li><a href="#">4</a></li>
+						<li><a href="#">5</a></li>
+						<li><a href="#">Next</a></li>
+					</ul>
+				</div>-->
+			</asp:Panel>
+
+			<asp:Panel id="parent_category" runat="server" Visible="false">
+				<div class="breadcrumbs">
+					<% this.Create_Breadcrumbs(); %>
+				</div>
+				
+				<br />
+
+				<p>Select a sub category to see some products</p>
+			</asp:Panel>
+
+			<asp:Panel id="product_listing" runat="server" Visible="false">
+				<div class="breadcrumbs">
+					<% this.Create_Breadcrumbs(); %>
+				</div>
+				
+				<br />
+
+				<ul class="thumbnails">
+					<asp:SqlDataSource ID="ProductsDS" runat="server" 
+						ConnectionString="<%$ ConnectionStrings:connString %>" 
+						SelectCommand="SELECT * FROM [products] WHERE ([category_id] = @category_id)">
+						<SelectParameters>
+							<asp:QueryStringParameter Name="category_id" QueryStringField="category" 
+								Type="Int32" />
+						</SelectParameters>
+					</asp:SqlDataSource>
+					<asp:Repeater ID="ProductsRepeater" runat="server" DataSourceID="ProductsDS">
+						<ItemTemplate>
+						<li class="span3">
+							<div class="thumbnail">
+								<img src="img/products/<%# Eval("product_code").ToString().Trim() %>.jpg" alt="">
+								<div class="caption">
+									<h4><%# Eval("product_name") %></h4>
+									<p><%# Eval("price") %></p>
+									<a class="btn btn-primary" href="./?product=<%# Eval("product_id") %>">View</a>
+									<a class="btn btn-success" href="./cart.aspx?action=add&product=<%# Eval("product_id") %>">Add to Cart</a>
+								</div>
+							</div>
+						</li>						
+						</ItemTemplate>
+					</asp:Repeater>
+				</ul>				
+			</asp:Panel>
+
+			<asp:Panel id="product_detail" runat="server" Visible="false">
+				<div class="breadcrumbs">
+					<% this.Create_Breadcrumbs_From_Product(Request.QueryString["product"]); %>
+				</div>
+				
+				<br />
+
+				<asp:SqlDataSource ID="ProductDetailDS" runat="server" 
+					ConnectionString="<%$ ConnectionStrings:connString %>" 
+					SelectCommand="SELECT * FROM [products] WHERE ([product_id] = @product_id)">
+					<SelectParameters>
+						<asp:QueryStringParameter Name="product_id" QueryStringField="product" 
+							Type="Int32" />
+					</SelectParameters>
+				</asp:SqlDataSource>
+				<asp:Repeater ID="ProductDetailRepeater" DataSourceID="ProductDetailDS" runat="server">
+				<ItemTemplate>
+				<div class="row">
+					<div class="span5">
+						<img class="media-object" src="img/products/<%# Eval("product_code").ToString().Trim() %>.jpg" alt="" />
+					</div>
+
+					<div class="span4">
+						<h4><%# Eval("product_name").ToString().Trim() %></h4>
+						<h5><%# Eval("product_code").ToString().Trim() %></h5>
+						<p>Sustainable squid try-hard, tousled pug freegan dolore pariatur nihil raw denim readymade whatever yr pop-up enim. Velit veniam McSweeney's whatever, next level street art brunch. Nesciunt delectus pork belly synth veniam. Velit brunch Terry Richardson anim. Occaecat butcher tousled 90's stumptown. Vinyl four loko semiotics readymade duis. Etsy Portland kitsch nihil ethical.</p>
+						<h4>$<%# Eval("price").ToString().Trim() %></h4>
+
+						<form>
+							<label>
+								<input type="text" id="quantity" name="quantity" value="1" class="span1" />&nbsp;Qty
+							</label>
+							<button class="btn btn-primary">Add to Chart</button>
+						</form>
+					</div>
+				</div>
+				</ItemTemplate>
+				</asp:Repeater>
+			</asp:Panel>
+			</div>
+		</div>
+	</div>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+	private String connString = ConfigurationManager.ConnectionStrings["connString"].ToString();
+	private SqlConnection conn;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+		this.conn = new SqlConnection(this.connString);
+		
+		int product = 0;
+		if(!String.IsNullOrEmpty(Request.QueryString["product"])) {
+			product = Convert.ToInt32(Request.QueryString["product"].ToString());
+			product_detail.Visible = true;
+			SubCategories.Visible = true;
+		}
+
+		if(product == 0)
+		{
+			int category = 0;
+
+			if(!String.IsNullOrEmpty(Request.QueryString["category"]))
+				category = Convert.ToInt32(Request.QueryString["category"].ToString());
+
+			if(category == 0) {
+				index.Visible = true;
+				SubCategories.Visible = false;
+			}
+			else {
+				index.Visible = false;
+
+				// If this category contains subcategories, let the user know that they need to pick a subcategory
+				SqlCommand cmd = new SqlCommand("SELECT * FROM [categories] WHERE parent_id = " + category, this.conn);
+				this.conn.Open();
+				SqlDataReader reader = cmd.ExecuteReader();
+			
+				if(reader.HasRows)
+					parent_category.Visible = true;
+				else
+					product_listing.Visible = true;
+
+				reader.Close();
+				this.conn.Close();
+			}
+		}
+    }
+
+	protected void Create_Breadcrumbs()
+	{
+		this.Create_Breadcrumbs(Convert.ToInt32(Request.QueryString["category"]), false);
+	}
+
+	protected void Create_Breadcrumbs_From_Product(String product)
+	{
+		int product_id = Convert.ToInt32(product);
+
+		SqlCommand cmd = new SqlCommand("SELECT category_id, product_name FROM [products] WHERE product_id = " + product_id, this.conn);
+		this.conn.Open();
+
+		SqlDataReader reader = cmd.ExecuteReader();
+		reader.Read();
+		int category_id = Convert.ToInt32(reader["category_id"].ToString());
+		String product_name = reader["product_name"].ToString();
+		this.conn.Close();
+
+		this.Create_Breadcrumbs(category_id, true);
+		Response.Write(product_name);
+	}
+
+	protected void Create_Breadcrumbs(int category_id, bool hyperlink_last)
+	{
+		if(hyperlink_last == null)
+			hyperlink_last = false;
+
+		if(String.IsNullOrEmpty(category_id.ToString()))
+			return;
+
+		if (category_id == 0)
+			return;
+
+		List<KeyValuePair<int,string>> breadcrumbs = new List<KeyValuePair<int,string>>();
+		
+		SqlCommand cmd = new SqlCommand("SELECT * FROM [categories] WHERE category_id = @id", this.conn);
+		cmd.Parameters.AddWithValue("@id", category_id);
+
+		this.conn.Open();
+		SqlDataReader reader = cmd.ExecuteReader();
+		
+		reader.Read();
+		int parent_id = Convert.ToInt32(reader["parent_id"].ToString());
+		breadcrumbs.Add(new KeyValuePair<int,string>(Convert.ToInt32(reader["category_id"].ToString()), reader["category_name"].ToString()));
+		reader.Close();
+
+		while(parent_id != 0)
+		{
+			SqlCommand catCmd = new SqlCommand("SELECT * FROM [categories] WHERE category_id = " + parent_id, this.conn);
+			SqlDataReader catReader = catCmd.ExecuteReader();
+			catReader.Read();
+
+			int crumb_id = Convert.ToInt32(catReader["category_id"].ToString());
+			String crumb_name = catReader["category_name"].ToString().Trim();
+
+			breadcrumbs.Add(new KeyValuePair<int, string>(crumb_id, crumb_name));
+			parent_id = Convert.ToInt32(catReader["parent_id"].ToString());
+
+			catReader.Close();
+		}
+
+		breadcrumbs.Add(new KeyValuePair<int, string>(0, "Home"));
+
+		for(int i = breadcrumbs.Count - 1; i > -1; i--)
+		{
+			if(i > 0)
+			{
+				String href = breadcrumbs[i].Key == 0 ? "Default.aspx" : "Default.aspx?category=" + breadcrumbs[i].Key;
+				Response.Write("<a href=\"" + href + "\">" + breadcrumbs[i].Value + "</a>");
+				Response.Write(" <span class=\"divider\">&raquo;</span> ");
+			}
+			else
+			{
+				if(!hyperlink_last)
+					Response.Write(breadcrumbs[i].Value);				
+				else
+				{
+					String href = breadcrumbs[i].Key == 0 ? "Default.aspx" : "Default.aspx?category=" + breadcrumbs[i].Key;
+					Response.Write("<a href=\"" + href + "\">" + breadcrumbs[i].Value + "</a>");
+					Response.Write(" <span class=\"divider\">&raquo;</span> ");
+				}
+			}
+		}
+	}
+
+	protected void Create_Sidebar()
+	{
+		int category_id = 0;
+
+		if(!String.IsNullOrEmpty(Request.QueryString["category"]))
+			category_id = Convert.ToInt32(Request.QueryString["category"]);
+		else if(!String.IsNullOrEmpty(Request.QueryString["product"]))
+		{
+			int product_id = Convert.ToInt32(Request.QueryString["product"]);
+
+			SqlCommand cmd = new SqlCommand("SELECT category_id, product_name FROM [products] WHERE product_id = " + product_id, this.conn);
+			this.conn.Open();
+
+			SqlDataReader reader = cmd.ExecuteReader();
+			reader.Read();
+			category_id = Convert.ToInt32(reader["category_id"].ToString());
+			this.conn.Close();
+		}
+
+		this.Create_Sidebar(category_id);
+	}
+
+	protected void Create_Sidebar(int category_id)
+	{
+		if (String.IsNullOrEmpty(category_id.ToString()))
+			return;
+
+		if (category_id == 0)
+			return;
+
+		SqlCommand cmd = new SqlCommand("SELECT * FROM [categories] WHERE category_id = " + category_id, this.conn);
+		this.conn.Open();
+
+		SqlDataReader reader = cmd.ExecuteReader();
+		reader.Read();
+
+		int parent_id = Convert.ToInt32(reader["parent_id"].ToString());
+		reader.Close();
+		
+		if(parent_id != 0)
+		{
+			while(parent_id != 0)
+			{
+				cmd = new SqlCommand("SELECT * FROM [categories] WHERE category_id = " + parent_id, this.conn);
+				reader = cmd.ExecuteReader();
+				reader.Read();
+
+				parent_id = Convert.ToInt32(reader["parent_id"].ToString());
+				if(parent_id == 0)
+				{
+					parent_id = Convert.ToInt32(reader["category_id"].ToString());
+					reader.Close();
+					break;
+				}
+
+				reader.Close();
+			}
+		}
+		else {
+			cmd = new SqlCommand("SELECT * FROM [categories] WHERE parent_id = " + category_id, this.conn);
+			reader = cmd.ExecuteReader();
+
+			if(reader.HasRows)
+				parent_id = category_id;
+			else
+				parent_id = 0;
+
+			reader.Close();
+		}
+
+		cmd = new SqlCommand("SELECT * FROM [categories] WHERE parent_id = " + parent_id, this.conn);
+		reader = cmd.ExecuteReader();
+		while(reader.Read())
+		{
+			int cat_id = Convert.ToInt32(reader["category_id"].ToString());
+			String cat_name = reader["category_name"].ToString().Trim();
+			String li_class = category_id == cat_id ? " class=\"active\"" : null;
+
+			Response.Write("<li" + li_class + "><a href=\"?category=" + cat_id + "\">" + cat_name + "</a></li>");
+		}
+
+		this.conn.Close();
+	}
+}
